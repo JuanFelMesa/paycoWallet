f149b082bf807bc11aa416df15f7fad26d13a2dc
ju4nr3v0l <jadamavecool@gmail.com>
2018-06-18 12:55:36 -0500

    creacion de usuario via SOAP servicio terminado

--- soap/src/Controller/TestSoapController.php
+++ soap/src/Controller/TestSoapController.php
@@ -19,15 +19,10 @@ class TestSoapController extends Controller
     public function test(Request $request){
 
         // Config
-        $client = new nusoap_client('http://soap.payco.com/index.php/usuario/soap/crear?wsdl', 'wsdl');
-        $client->setEndpoint('http://soap.payco.com/index.php/usuario/soap/crear');
-//        $client->soap_defencoding = 'ISO-8859-1';
-        $client->decode_utf8 = true;
-//        $client->debug();
-//        echo '<pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
-//        echo '<h2>Response</h2>';
-//        echo '<pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
+        $client = new nusoap_client('http://soap.payco.com/index.php/usuario/soap?wsdl', 'wsdl');
+        $client->setEndpoint('http://soap.payco.com/index.php/usuario/soap');
 
+        $client->decode_utf8 = true;
 
         // Calls
         $result = $client->call('crearusuario', array(
@@ -39,30 +34,7 @@ class TestSoapController extends Controller
 
         ));
 
-//
-//
-//
-//
-//
-//
-//        $soapClient = new \SoapClient('http://soap.payco.com/index.php/usuario/soap?wsdl');
-//        $soapClient->setEndpoint('http://soap.payco.com/index.php/usuario/soap');
-//
-//        $result = $soapClient->crearusuario(json_encode( array(
-//            'nombre'=>  'Juan David Marulanda V.',
-//            'correo' => 'juandavidmarulanda@yahoo.com',
-//            'numeroIdentificacion' => '15371377',
-//            'celular'=>'3185668227'
 
-
-//        )));
-        $response = new Response();
-        $response->headers->set('Content-Type', 'text/xml; charset=UTF-8');
-//
-//        ob_start();
-//
-//
-        $response->setContent($result);
-        return $response;
+        return $result;
     }
 }
